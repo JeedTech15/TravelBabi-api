5942fc510a83fd7eff2c3174d970001f8ee0107d
Emmanuel Bamidélé <165570846+Programmer-Emmanuel@users.noreply.github.com>
2026-01-15 09:15:22 +0000

    Début de TravelBabi | 15/01/2025

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Model
 {
+    use HasApiTokens, HasFactory;
     protected $fillable = ['nom','numero','image', 'email'];
 
     public $incrementing = false; // empêche l'auto-incrémentation
