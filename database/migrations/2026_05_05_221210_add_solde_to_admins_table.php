32d582f29f4dd5bb06c67201bb8daca6e45933f0
Gab705 <angeeloge8@gmail.com>
2026-05-06 20:11:21 +0000

    Modifcation AdminController

--- database/migrations/2026_05_05_221210_add_solde_to_admins_table.php
+++ database/migrations/2026_05_05_221210_add_solde_to_admins_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('admins', function (Blueprint $table) {
-            $table->decimal('solde', 10, 2)->default(0)->after('role');
+            $table->decimal('solde', 10, 2)->default(0)->after('role')->nullable();
         });
     }
 
